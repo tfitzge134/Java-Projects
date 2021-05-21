e5a95ec66da16336665034c6cc6a5dd8ad4b0bf5
Teresa Fitzgerald <teresafitzgerald@Teresas-MBP.lan>
2021-05-20 22:11:56 -0500

    REMOVE Servlet mapping in web.xml AND ADDED in Servlet java file

--- Employee-Reimbursment-System-MAVEN/src/main/java/com/revature/controllers/FrontControllerServlet.java
+++ Employee-Reimbursment-System-MAVEN/src/main/java/com/revature/controllers/FrontControllerServlet.java
@@ -21,7 +21,7 @@ import com.revature.util.HtmlUtil;
 /**
  * Servlet implementation class HelloServlet
  */
-//@WebServlet("/ers")
+@WebServlet("/ers")
 public class FrontControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
